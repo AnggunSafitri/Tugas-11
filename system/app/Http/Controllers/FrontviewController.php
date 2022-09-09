b6d6c587a528f023bb1bfab36eb4967920fe547b
AnggunSafitri <anggunsafitri2211@gmail.com>
2022-09-09 16:33:52 +0700

    Tugas-11

---
+++ system/app/Http/Controllers/FrontviewController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Produk;
+use Illuminate\Http\Request;
+
+class FrontviewController extends Controller
+{
+    
+    public function index()
+    {
+        $data['list_produk'] = Produk::all();
+        return view('frontview.index', $data);
+    }
+
+    public function account()
+    {
+        return view('frontview.account');
+    }
+
+
+    public function produk()
+    {
+        $data['list_produk'] = Produk::all();
+        return view('frontview.produk', $data);
+    }
+
+    public function produkdetail()
+    {
+        return view('frontview.produkdetail');
+    }
+
+    public function testCollection()
+    {
+        $data['list_produk'] = Produk::all();
+
+       return view('test-collection', $data);
+    }
+   
+}
